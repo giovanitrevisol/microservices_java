f19118c42b4fce381b222eceb99a297661fe2d2a
Giovani Trevisol <giovanitrevisol93@gmail.com>
2021-03-27 16:39:32 -0300

    hr-worker implementatation

---
+++ hr-worker/src/main/java/com/gt/hrworker/resources/WorkerResources.java
@@ -0,0 +1,29 @@
+package com.gt.hrworker.resources;
+
+import com.gt.hrworker.entities.Worker;
+import com.gt.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResources {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll(){
+        List<Worker> list = workerRepository.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker obj = workerRepository.findById(id).get();
+        return ResponseEntity.ok().body(obj);
+    }
+}
