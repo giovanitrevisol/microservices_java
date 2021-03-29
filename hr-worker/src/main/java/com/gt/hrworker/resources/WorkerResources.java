be623a47b553e251363005b222a6dba5520fc7f9
giovani.trevisol <giovani.trevisol@cwi.com.br>
2021-03-29 17:39:19 -0300

    ribbon balance

--- hr-worker/src/main/java/com/gt/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/gt/hrworker/resources/WorkerResources.java
@@ -2,15 +2,25 @@ package com.gt.hrworker.resources;
 
 import com.gt.hrworker.entities.Worker;
 import com.gt.hrworker.repositories.WorkerRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResources {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -23,6 +33,8 @@ public class WorkerResources {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("Port = " + env.getProperty(("local.server.port")));
         Worker obj = workerRepository.findById(id).get();
         return ResponseEntity.ok().body(obj);
     }
