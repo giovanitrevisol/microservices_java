521e1891aa353fb985a8798ad6f80aef271143d8
giovani.trevisol <giovani.trevisol@cwi.com.br>
2021-04-08 19:27:37 -0300

    criando e configurando hr-config

--- hr-worker/src/main/java/com/gt/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/gt/hrworker/resources/WorkerResources.java
@@ -6,24 +6,36 @@ import com.gt.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResources {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfig(){
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
@@ -38,4 +50,6 @@ public class WorkerResources {
         Worker obj = workerRepository.findById(id).get();
         return ResponseEntity.ok().body(obj);
     }
+
+
 }
