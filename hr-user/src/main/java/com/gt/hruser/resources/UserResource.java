e637cacf727678b1a25691dd6c7b7fd60e210722
Giovani Trevisol <giovanitrevisol93@gmail.com>
2021-04-20 22:19:54 -0300

    userResource ok

--- hr-user/src/main/java/com/gt/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/gt/hruser/resources/UserResource.java
@@ -4,20 +4,24 @@ import com.gt.hruser.entities.User;
 import com.gt.hruser.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
-    public ResponseEntity<User> findByid(@PathVariable Long id){
-        User obj = userRepository.findById(id).get();
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
 
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User obj = repository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
 }
